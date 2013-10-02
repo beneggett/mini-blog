a62fa28a4af00671353aa9f0ca1ff983f9fef3cb
Ben Eggett <beneggett@gmail.com>
2013-10-01 18:34:44 -0600

    Minor formatting

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ MiniBlog::Application.routes.draw do
 
   # get '/posts', to: "posts#index", as: :posts
   # get '/posts/new', to: "posts#new", as: :new_post
-  # post '/posts', to: "posts#create" #, as: :create_post
-  get '/posts/:id', to: "posts#show", as: :post
+  # post '/posts', to: "posts#create"
+  # get '/posts/:id', to: "posts#show", as: :post
   # get '/posts/:id/edit', to: "posts#edit", as: :edit_post
-  # put '/posts/:id/update', to: "posts#update" #, as: :update_post
-  # delete '/posts/:id', to: "posts#destroy" #, as: :delete_post
+  # put '/posts/:id/update', to: "posts#update"
+  # delete '/posts/:id', to: "posts#destroy"
 
   # root :to => 'home#index'
   root to: 'home#index'
