ce9719104ca2f0861e282bcf6c2aa622e3caeb86
Ben Eggett <beneggett@gmail.com>
2013-10-04 16:03:04 -0600

    Showing advanced associations, nested associations, custom associations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ActiveRecord::Base
   has_many :posts
+  has_many :comments, through: :posts
   attr_accessible :bio, :email, :first_name, :last_name, :twitter, :username
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :author
-  has_many :comments  
+  has_many :comments
+  has_many :notes, :class_name => "Comment", 
+  :inverse_of => :post, :foreign_key => :post_id, :primary_key => :id
   attr_accessible :body, :published_at, :title, :author_id
 end
